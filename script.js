96f42856b3cab0b15dbc2db599bfa2277b67e289
ryusei <r.aoooki27@gmail.com>
2021-01-20 22:09:59 +0900

    first-commit

---
+++ script.js
@@ -0,0 +1,12 @@
+$(function() {
+    $(".menu-icon").click(function() {
+        $("#clickedMenu-modal").fadeIn()
+
+    });
+
+    $(".close-modal").click(function() {
+        $("#clickedMenu-modal").fadeOut()
+    });
+
+
+});
